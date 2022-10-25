40a26e393d8ea492b981d7745111ce8be08ccfa6
UsernameViktoriia <tori-boss@ukr.net>
2022-10-25 18:45:10 +0300

    виконано 1 задачу

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
-console.log('test');
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
+
+btnStop.setAttribute('disabled', 'disabled');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+// const color = getRandomHexColor();
+
+const onClickStart = () => {
+  body.style.backgroundColor = getRandomHexColor();
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  btnStart.setAttribute('disabled', 'disabled');
+  btnStop.removeAttribute('disabled', 'disabled');
+};
+const onClickStop = () => {
+  clearInterval(timerId);
+  btnStop.setAttribute('disabled', 'disabled');
+  btnStart.removeAttribute('disabled', 'disabled');
+};
+
+btnStart.addEventListener('click', onClickStart);
+btnStop.addEventListener('click', onClickStop);
